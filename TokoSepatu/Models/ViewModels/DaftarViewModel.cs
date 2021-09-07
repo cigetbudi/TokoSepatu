1020f0533d4184127701b669c3bba12cd8aa9dae
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-09-07 17:42:31 +0700

    RegisterViewModel

---
+++ TokoSepatu/Models/ViewModels/DaftarViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DepotSneakers.Models.ViewModels
+{
+    public class DaftarViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = " {0} harus minimal {2} karakter.", MinimumLength = 6)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Konfirmasi Password")]
+        [Compare("Password", ErrorMessage = "harus diisi sama dengan password.")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+    }
+}
