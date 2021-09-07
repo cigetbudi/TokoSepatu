eddf041294b9b5e72b8de38ac09dc1a8d2683f81
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-09-07 17:27:39 +0700

    loginViewModel

---
+++ TokoSepatu/Controllers/AkunController.cs
@@ -0,0 +1,28 @@
+﻿using TokoSepatu.Models;
+using TokoSepatu.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TokoSepatu.Controllers
+{
+    public class AkunController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AkunController(ApplicationDbContext db)
+        {
+            _db = db;
+
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
