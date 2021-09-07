a1d10a3e267c76798ea9781ae919857c7acfb71a
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-09-07 19:19:45 +0700

    Daftar Post Action Method

--- TokoSepatu/Controllers/AkunController.cs
+++ TokoSepatu/Controllers/AkunController.cs
@@ -6,17 +6,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TokoSepatu.Models.ViewModels;
 
 namespace TokoSepatu.Controllers
 {
     public class AkunController : Controller
     {
         private readonly ApplicationDbContext _db;
-
-        public AkunController(ApplicationDbContext db)
+        UserManager<ApplicationUser> _userManager;
+        SignInManager<ApplicationUser> _signInManager;
+        RoleManager<IdentityRole> _roleManager;
+        public AkunController(ApplicationDbContext db, UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager, SignInManager<ApplicationUser> signInManager)
         {
             _db = db;
-
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
 
         }
         public IActionResult Login()
@@ -26,6 +32,29 @@ namespace TokoSepatu.Controllers
         public IActionResult Daftar()
         {
             return PartialView();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Daftar(DaftarViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name
+                };
+
+                var result = await _userManager.CreateAsync(user);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+
+            }
+            return View();
         }
     }
 }
