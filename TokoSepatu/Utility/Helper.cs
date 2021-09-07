5786582050e8c7b2a4003e7f9e1d7629d1aa3c54
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-09-07 17:45:30 +0700

    Helper Class

---
+++ TokoSepatu/Utility/Helper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TokoSepatu.Utility
+{
+    public class Helper
+    {
+        public static string Owner = "Owner";
+        public static string Admin = "Admin";
+
+        public static List<SelectListItem> GetRolesFromDropDown()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem{Value=Helper.Owner,Text=Helper.Owner},
+                new SelectListItem{Value=Helper.Admin,Text=Helper.Admin},
+            };
+        }
+    }
+}
