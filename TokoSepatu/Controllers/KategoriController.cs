71d1873f8edf833e01ee73d2e753dfdbba326e9b
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-09-09 00:04:08 +0700

    KategoriController dan Index View

---
+++ TokoSepatu/Controllers/KategoriController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TokoSepatu.Models;
+
+namespace TokoSepatu.Controllers
+{
+    public class KategoriController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public KategoriController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Models.Kategori> objList = _db.Kategoris;
+            return View(objList);
+        }
+    }
+}
