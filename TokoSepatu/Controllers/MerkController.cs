155c857548727de3669092de43304154374c711a
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-09-08 14:13:59 +0700

    Merk View

---
+++ TokoSepatu/Controllers/MerkController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TokoSepatu.Models;
+
+namespace TokoSepatu.Controllers
+{
+    public class MerkController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public MerkController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            //LIST
+            IEnumerable<Models.Merk> objList = _db.Merks;
+            return View(objList);
+        }
+
+        public IActionResult Create()
+        {
+            return PartialView();
+        }
+    }
+}
