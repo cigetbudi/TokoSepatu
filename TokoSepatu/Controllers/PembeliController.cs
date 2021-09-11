f7f59867b72ce103eba5d55f333465585a1e3101
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-09-11 22:59:05 +0700

    Pembeli Create GET POST dan View

---
+++ TokoSepatu/Controllers/PembeliController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TokoSepatu.Models;
+
+namespace TokoSepatu.Controllers
+{
+    public class PembeliController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public PembeliController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            //List
+            IEnumerable<Pembeli> objList = _db.Pembelis;
+            return View(objList);
+        }
+
+        public IActionResult Create()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Create (Pembeli obj)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _db.Pembelis.Add(obj);
+                    _db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            catch (Exception)
+            {
+
+                ModelState.AddModelError("", "gagal saat menambah data");
+            }
+            return View(obj);
+        }
+
+       
+    
+    }
+}
