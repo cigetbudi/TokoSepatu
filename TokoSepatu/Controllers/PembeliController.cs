d8f337ba765a0a0973378c66747724f487c63824
Sigit Budiarmawan <cigetbudi@outlook.com>
2021-09-11 23:15:40 +0700

    Pembeli Delete Update GET POST dan View

--- TokoSepatu/Controllers/PembeliController.cs
+++ TokoSepatu/Controllers/PembeliController.cs
@@ -47,7 +47,77 @@ namespace TokoSepatu.Controllers
             return View(obj);
         }
 
-       
-    
+        //GET HAPUS
+        public IActionResult Hapus(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _db.Pembelis.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return PartialView(obj);
+        }
+
+        //POST HAPUS
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult HapusPost(int? id)
+        {
+            var obj = _db.Pembelis.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _db.Pembelis.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        //GET Rubah
+        public IActionResult Rubah(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _db.Pembelis.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return PartialView(obj);
+        }
+
+        //POST Rubah
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Rubah(Pembeli obj)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _db.Pembelis.Update(obj);
+                    _db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            catch (Exception)
+            {
+
+                ModelState.AddModelError("", "gagal saat mengdit data");
+            }
+            return View(obj);
+        }
+
+
     }
 }
